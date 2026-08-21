38e9ecd82de0c3aec34a6ef6e861806836d1c620
codArtic0 <raulsousadesigner@gmail.com>
2026-08-20 21:52:56 -0300

    chore: atualiza versao para v1.0.0

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ def subtracao(a,b):
     return a-b;
 
 if __name__ == "__main__":
-    print("Calculadora inicial: ")
+    print("Calculadora v1.0.0: ")
     print()
     a = int(input("Digite um número: "))
     b = int(input("Digite outro número: "))
