dcb46c7247e0a3b123e9c14c9b0f1d518acea08b
codArtic0 <raulsousadesigner@gmail.com>
2026-08-20 22:07:08 -0300

    feat: adiciona funcao de multiplicacao

--- calc.py
+++ calc.py
@@ -4,6 +4,9 @@ def soma(a,b):
 def subtracao(a,b):
     return a-b;
 
+def multiplicacao(a,b):
+    return a*b;
+
 if __name__ == "__main__":
     print("Calculadora v1.0.0: ")
     print()
@@ -11,7 +14,9 @@ if __name__ == "__main__":
     b = int(input("Digite outro número: "))
     resultSoma = soma(a,b)
     resultSubtracao = subtracao(a,b)
+    resultMultiplicacao = multiplicacao(a,b)
     print()
     
     print("Soma: " + str(resultSoma))
     print("Subtracao: " + str(resultSubtracao))
+    print("Multiplicacao: " + str(resultMultiplicacao))
