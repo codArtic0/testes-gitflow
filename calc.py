56c07b552d4204d9aeca12db102bb7d87bf4f14a
codArtic0 <raulsousadesigner@gmail.com>
2026-08-20 22:09:50 -0300

    chore: atualiza versao para v1.1.0

--- calc.py
+++ calc.py
@@ -8,7 +8,7 @@ def multiplicacao(a,b):
     return a*b;
 
 if __name__ == "__main__":
-    print("Calculadora v1.0.0: ")
+    print("Calculadora v1.1.0: ")
     print()
     a = int(input("Digite um número: "))
     b = int(input("Digite outro número: "))
